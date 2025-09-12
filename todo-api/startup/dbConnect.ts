1965e87821a9564e27d24a9732a93b40076e7d9c
pesekt1 <pesekt@gmail.com>
2025-09-12 11:30:56 +0200

    added a log

--- todo-api/startup/dbConnect.ts
+++ todo-api/startup/dbConnect.ts
@@ -4,6 +4,7 @@ export default () => {
   if (!process.env.MONGO_URI) {
     throw new Error("MONGO_URI is not defined");
   }
+  console.log("Connecting to MongoDB... " + process.env.MONGO_URI);
   mongoose
     .connect(process.env.MONGO_URI)
     .then(() => console.log("Connected to MongoDB"))
